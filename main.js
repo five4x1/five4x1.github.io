4f6bba5d047fadc6b0ca3e63a3d73419d183a3f3
five4x1 <84310970+five4x1@users.noreply.github.com>
2021-05-18 22:06:21 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 // main JS for 54x1
 $(document).ready(function() {
-  if ($(".items>div>a").length > 0) {
-    $("#flex-container>#funcBatch").show();
-  } else {
-    $("#flex-container>#funcBatch").hide();
-  }
-if ($(".paginate-container>*").length > 0){
-    $('.paginate-container').show();
-  } else {
-    $('.paginate-container').hide();
-  }
+      if ($(".items>div>a").length > 0) {
+        $("#flex-container>#funcBatch").show();
+      } else {
+        $("#flex-container>#funcBatch").hide();
+      }
+      if ($(".paginate-container>*").length > 0) {
+        $('.paginate-container').show();
+      } else {
+        $('.paginate-container').hide();
+      }
 
-  
-  
-  $('.items').css({"box-shadow":"none"});
-  
-  $('body').mousemove(function(e) {
-    var x = (e.pageX * -1 / 15);
-    var y = (e.pageY * -1 / 15);
-    $('section').css('background-position', x + 'px ' + y + 'px');
-  });
-                    
-});
+      $(".intro markdown-body").empty();
+      $(".intro markdown-body").append('<div class"image-wrapper"></div>');
 
-function funcBatch(){document.getElementById("funcBatch").href="https://jfofbz.deta.dev/index?url="+document.URL;return false;};
+      $('.items').css({
+        "box-shadow": "none"
+      });
+      $(window).on("load", function() {
+          $('body').mousemove(function(e) {
+            var x = (e.pageX * -1 / 15);
+            var y = (e.pageY * -1 / 15);
+            $('section').css('background-position', x + 'px ' + y + 'px');
+          });
+        }
+
+      });
+
+    function funcBatch() {
+      document.getElementById("funcBatch").href = "https://jfofbz.deta.dev/index?url=" + document.URL;
+      return false;
+    };
